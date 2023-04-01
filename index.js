c8e027258cbdb0d0b26909322a72f24330b21024
Antonio Trinidad Mercedes <ambioristm@gmail.com>
2023-04-01 16:32:45 -0400

    agregamos la lista de productos dinamicamente

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerIcon.addEventListener('click', toggleMobileMenu);
@@ -37,3 +38,57 @@ function toggleCarritoAside(){
   aside.classList.toggle('inactive');
   desktopMenu.classList.add('inactive');
 }
+
+const productList = [];
+productList.push({
+  name: 'Bike',
+  price: 120,
+  Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Laptop Acer',
+  price: 570,
+  Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Camisa',
+  price: 22,
+  Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+//Productos creados dinamicamente.
+function renderProducts(arr) {
+  for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.Image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.append(productPrice, productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productInfoImg = document.createElement('img');
+    productInfoImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productInfoImg);
+  
+    productInfo.append(productInfoDiv, productInfoFigure);
+  
+    productCard.append(productImg, productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
