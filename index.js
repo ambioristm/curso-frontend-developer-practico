4b674461416114e7f8c2594d7d98fbb32f0c4929
Antonio Trinidad Mercedes <ambioristm@gmail.com>
2023-04-01 21:40:49 -0400

    agregamos el componente detalle del producto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const burguerIcon = document.querySelector('.menu');
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,19 +11,19 @@ burguerIcon.addEventListener('click', toggleMobileMenu);
 shoppingCartIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
   mobileMenu.classList.toggle('inactive');
 }
@@ -33,9 +33,9 @@ function toggleCarritoAside(){
   
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive');
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
   }
-  aside.classList.toggle('inactive');
+  shoppingCartContainer.classList.toggle('inactive');
   desktopMenu.classList.add('inactive');
 }
 
